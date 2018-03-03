8a355fe98ee3561ae1aad3c2c09f8f6d2a707ac0
Kirill <kirillmay@gmail.com>
2018-03-03 12:01:19 -0600

    Added TestNG
    Add testing.xml

--- Automation/src/test/java/com/dice/FirstTest.java
+++ Automation/src/test/java/com/dice/FirstTest.java
@@ -10,13 +10,27 @@ public class FirstTest {
 	@Test
 	public void firstTestMethod() {
 		System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+		WebDriver driver = new ChromeDriver(); 
 		//System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
 		//WebDriver driver = new FirefoxDriver(); 
-		WebDriver driver = new ChromeDriver(); 
 		
 		//Open dice.com
 		driver.get("http://dice.com");
 		System.out.println("Dice opened. Test Passed!");
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void secondTestMethod() {
+		System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+		WebDriver driver = new ChromeDriver(); 
+		//System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver(); 
+		
+		//Open linkedin.com
+		driver.get("http://linkedin.com");
+		System.out.println("Linkedin opened. Test Passed!");
 		
 		driver.quit();
 	}
